07f64a2cf2672c239c52df1e61c8119301bfe7ff
Ranjith K <thisisranjithk@gmail.com>
2024-11-27 15:10:57 +0530

    fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,27 +2,44 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue } from "../api/data";
+import { revenue } from "../lib/placeholder-data";
+import connectDB from "@/config/db";
+import Invoice from "../../models/Invoices";
+import Customer from "../../models/Customers.js";
 
 export default async function Page() {
+  await connectDB();
+  const latestInvoices = await Invoice.find({})
+    .sort({ createdAt: -1 })
+    .limit(5);
+  const Invoices = await Invoice.find({}).lean();
+  const Customers = await Customer.find({}).lean();
+
+  const totalPendingInvoices = await Invoice.countDocuments({
+    status: "pending",
+  });
+  const totalPaidInvoices = await Invoice.countDocuments({ status: "paid" });
+
+  const numberOfInvoices = Invoices?.length;
+  const numberOfCustomers = Customers?.length;
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue} /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,7 +2,8 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
-import { LatestInvoice } from "@/app/api/definitions";
+import { LatestInvoice } from "@/app/lib/definitions";
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -15,20 +16,19 @@ export default async function LatestInvoices({
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
-                  },
+                    "border-t": i !== 0,
+                  }
                 )}
               >
-                <div className="flex items-center">
+                {/* <div className="flex items-center">
                   <Image
                     src={invoice.image_url}
                     alt={`${invoice.name}'s profile picture`}
@@ -44,7 +44,7 @@ export default async function LatestInvoices({
                       {invoice.email}
                     </p>
                   </div>
-                </div>
+                </div> */}
                 <p
                   className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
                 >
@@ -53,7 +53,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
